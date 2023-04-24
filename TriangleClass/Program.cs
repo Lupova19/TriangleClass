33b92b82943e983485f683146ae4a2e2a7c436b3
Lupova19 <nadiqlupova@gmail.com>
2023-04-24 19:42:30 +0300

    first commit

---
+++ TriangleClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vavedi A = ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Vavedi H = ");
+            double h = double.Parse(Console.ReadLine());
+
+            Triangle triangle1 = new Triangle(a, h);
+
+            List<Triangle> triangles = new List<Triangle>();
+            triangles.Add(triangle1);
+
+            triangle1.AreaOfTriangle();
+            triangle1.PrintTriangle();
+            triangle1.Lenght();
+            triangle1.Height();
+        }
+    }
+}
---
+++ TriangleClass/Triangle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleClass
+{
+    internal class Triangle
+    {
+
+        private double a;
+        private double h;
+
+        //public double A { get; set; }
+        //public double H { get; set; }
+
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        public double H
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                h = value;
+            }
+        }
+
+        public Triangle(double a, double h)
+        {
+            this.a = a;
+            this.h = h;
+        }
+
+        //public Triangle()
+        //{
+        //    this.A = 0;
+        //    this.H = 0;                
+        //}
+
+        public double AreaOfTriangle()
+        {
+            return a * h / 2;         // s = ?; s = a * h / 2;!!!
+        }
+        public void PrintTriangle()
+        {
+            Console.WriteLine($"Stranata e {this.A}, visocinata kam neq e {this.H} s lice {AreaOfTriangle()}!");
+        }
+        public double Lenght()
+        {
+            return a;
+        }
+        public double Height()
+        {
+            return h;
+        }
+    }
+}
